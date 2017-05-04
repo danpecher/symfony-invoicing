653ef79925c3467acc429aadf6e81e82d3792450
Dan Pecher <dan.pecher@gmail.com>
2017-05-04 12:49:06 -0500

    update invoice items form

--- web/js/invoice.js
+++ web/js/invoice.js
@@ -1,33 +1,58 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    const defaultItem = {unit: 'ks', title: '', quantity: 1, pricePerUnitCents: 0};
+
     Vue.component('invoice-items', {
         props: ['initialItems'],
-        template: `<div class="invoice-items-container section">
-            <div v-for="(item, index) in items" class="columns">
-                <select v-model="item.unit" :name="'invoice_form[items][' + index + '][unit]'">
-                    <option value="ks">ks</option>
-                    <option value="kg">kg</option>
-                </select>
-                <input :name="'invoice_form[items][' + index + '][quantity]'" v-model="item.quantity" type="text" class="column" placeholder="Počet">
-                <input :name="'invoice_form[items][' + index + '][title]'" v-model="item.title" type="text" class="column" placeholder="Název">
-                <input :name="'invoice_form[items][' + index + '][pricePerUnitCents]'" v-model="item.pricePerUnitCents" type="text" class="column" placeholder="Cena">
-                <a href="" @click.prevent="removeItem(index)"><i class="fa fa-trash"></i></a>
+        template: `<div class="invoice-items-container">
+            <div v-for="(item, index) in items" class="invoice-item-row">
+                <input :name="'invoice_form[items][' + index + '][quantity]'" v-model="item.quantity" type="number" class="input unit-input" placeholder="Počet">
+                <span class="select">
+                    <select v-model="item.unit" :name="'invoice_form[items][' + index + '][unit]'">
+                        <option value="ks">ks</option>
+                        <option value="kg">kg</option>
+                    </select>
+                </span>
+                <input :name="'invoice_form[items][' + index + '][title]'" v-model="item.title" type="text" class="input" placeholder="Název">
+                <input :name="'invoice_form[items][' + index + '][pricePerUnitCents]'" v-model="item.pricePerUnitCents" type="text" class="input price-input" placeholder="Cena">
+                <a href="" @click.prevent="removeItem(index)" class="button is-small is-danger"><i class="fa fa-trash"></i></a>
             </div>
-            <a @click.prevent="addItem" href="" class="button is-success is-small">Přidat položku</a>
+            <div class="invoice-add-item">
+                <a @click.prevent="addItem" href="" class="button is-success is-small">
+                    <span class="icon is-small">
+                           <i class="fa fa-plus"></i> 
+                    </span>
+                    <span>Přidat položku</span>
+                </a>
+            </div>
+            <div class="invoice-summary">
+                <div class="invoice-total">Celkem <strong>{{ total }} {{ currency }}</strong></div>
+            </div>
         </div>`,
         data: function () {
-            let defaultItem = {unit: 'ks', title: '', quantity: 1, pricePerUnitCents: 0};
             return {
-                items: this.initialItems.length < 1 ? [defaultItem] : this.initialItems
+                items: this.initialItems.length < 1 ? [Object.assign({}, defaultItem)] : this.initialItems,
+                currency: '',
+            }
+        },
+        computed: {
+            total: function (vm) {
+                return vm.items.reduce((acc, item) => acc + item.pricePerUnitCents * item.quantity, 0);
             }
         },
         methods: {
             addItem: function () {
-                this.items.push({});
+                this.items.push(Object.assign({}, defaultItem));
             },
             removeItem: function (index) {
                 this.items.splice(index, 1);
-                this.items = this.items;
             }
+        },
+        mounted: function () {
+            const $currencyInput = document.querySelector('#invoice_form_currency');
+            this.currency = $currencyInput.value;
+            $currencyInput.addEventListener('change', (e) => {
+                this.currency = e.target.value;
+            });
         }
     });
 
